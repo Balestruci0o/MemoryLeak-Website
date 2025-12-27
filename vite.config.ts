35f936ac47f8d514994c4115d3b3ebca94261a74
Balestrucioo <matin.pavlik0314@gmail.com>
2025-12-28 00:49:29 +0100

    možno

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,25 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-});
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+}));
